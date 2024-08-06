c3807024cfb02c90852709340ca094a8a75ed259
xbeastgameplay <160731032+xbeastgameplay@users.noreply.github.com>
2024-08-06 17:06:26 -0300

    Add files via upload

---
+++ 04/Funcionario.java
@@ -0,0 +1,11 @@
+public class Funcionario {
+    private int horasTrabalhadas;
+
+    public Funcionario(){
+        horasTrabalhadas=0;
+
+    }
+    public int getHorasTrabalhadas(){
+        return horasTrabalhadas;
+    }
+}
---
+++ 04/Retangulo.java
@@ -0,0 +1,19 @@
+public class Retangulo {
+    public double getBase() {
+        return base;
+    }
+    public void setBase(double base) {
+        this.base = base;
+    }
+    private double base;
+    private double altura;
+    public static void main(String[] args) {
+        
+    }
+    public double getAltura() {
+        return altura;
+    }
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+}
